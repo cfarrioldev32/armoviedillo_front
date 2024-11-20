77393ed7d6f9ab35b7333d13f6852886ddb4b4da
cfarrioldev32 <cristianfarriol28@gmail.com>
2024-11-20 12:58:41 +0100

    Update movies.service.ts

--- src/app/features/movies/services/movies.service.ts
+++ src/app/features/movies/services/movies.service.ts
@@ -13,14 +13,14 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
 
   getPopular(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/popular`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/popular?api_key=9e48c1f75a9af148785c9deba8810bb2&language=en-US&page=1`);
   }
 
   getNowPlaying(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/now-playing`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/now-playing?api_key=9e48c1f75a9af148785c9deba8810bb2&language=en-US&page=1`);
   }
 
   getMovieCredits(movieId: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/credits/${movieId}`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/{movieId}/credits?api_key=9e48c1f75a9af148785c9deba8810bb2`);
   }
 }
